c7093d1c6d941ace18d2681e6e22c0dee0eb4dbe
Camilo Duran <camilo.duran@usm.cl>
2022-11-03 17:10:10 -0300

    Commit Segundo: ejercicio tema 9

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        Persona persona = new Persona();
-        persona.setEdad(20);
-        persona.setNombre("Camilo");
-        persona.setTelefono("+569 24981209");
+    public static void main(String[] args) throws Exception {    
+        Cliente cliente = new Cliente();
+        cliente.setEdad(11);
+        cliente.setNombre("Pedro");
+        cliente.setTelefono("+569 24981209");
+        cliente.setCredito(false);
 
-        System.out.println(persona.getNombre() + " " + "tiene " + persona.getEdad() + " agnos" + " y su numero de telefono es: " + persona.getTelefono());       
+        System.out.println(cliente.getNombre() + " " + "tiene " + cliente.getEdad() + " agnos" + " y su numero de telefono es: " + cliente.getTelefono() + " .Posee linea de credito: " + cliente.isCredito());
+
+        Trabajador trabajador = new Trabajador();
+        trabajador.setEdad(30);
+        trabajador.setNombre("Tamara");
+        trabajador.setTelefono("+569 43452134");
+        trabajador.setSalario(542300);
+
+        System.out.println(trabajador.getNombre() + " " + "tiene " + trabajador.getEdad() + " agnos" + " y su numero de telefono es: " + trabajador.getTelefono() + " .Posee $ " + trabajador.getSalario() + " de salario.");
     }
 }
 
@@ -15,7 +24,7 @@ class Persona{
     private String telefono;
 
     public Persona() {
-        //Constructor sin parametros
+        System.out.println("Estoy en el contructor de Persona.");
     }
 
     public void setEdad(int edad){
@@ -37,5 +46,35 @@ class Persona{
     }
     public String getTelefono(){
         return this.telefono;
+    }
+}
+
+class Cliente extends Persona{
+    private boolean credito;
+
+    public Cliente() {
+        System.out.println("Estoy en el contructor de Cliente.");
+    }
+
+    public void setCredito(boolean credito){
+        this.credito = credito;
+    }
+    public boolean isCredito(){       
+        return this.credito;
+    }
+}
+
+class Trabajador extends Persona{
+    private int salario;
+
+    public Trabajador() {
+        System.out.println("Estoy en el contructor de Trabajador.");
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+    public int getSalario() {
+        return this.salario;
     }
 }
